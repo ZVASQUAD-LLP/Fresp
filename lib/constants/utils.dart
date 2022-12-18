9f7b64397cc8289cab290c533b6d3eff6ddd8c4b
Sougata Hanra <hanras97@gmail.com>
2022-12-18 21:12:29 +0100

    test

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -2,10 +2,16 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fresp/constants/global_variables.dart';
 
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text(text)),
+    SnackBar(
+      content: Text(text),
+      backgroundColor: GlobalVariables.secondaryColor,
+      behavior: SnackBarBehavior.floating,
+      duration: Duration(milliseconds: 3000),
+    ),
   );
 }
 
